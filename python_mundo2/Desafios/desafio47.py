371c4fbc78f5fd16485056dfdbd84697103e830e
jefersonar-cmd <jefersonaraujo86@gmail.com>
2021-02-13 23:43:11 -0300

    Desafio 47 feito

---
+++ python_mundo2/Desafios/desafio47.py
@@ -0,0 +1,7 @@
+#crie um programa que mostre na tela
+#todos os número pares que estao no intervalo entre 1 a 50
+print('numeros pares de 1 a 50')
+for c in range(1, 50+1):
+    if c % 2 == 0:
+        print(c)
+print('FIM')
