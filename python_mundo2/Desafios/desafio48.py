6615652e677f004327aa07e8ba12f2438824d3a6
jefersonar-cmd <jefersonaraujo86@gmail.com>
2021-02-16 19:37:48 -0300

    Desafio 48 reajustado

--- python_mundo2/Desafios/desafio48.py
+++ python_mundo2/Desafios/desafio48.py
@@ -2,9 +2,12 @@
 # a soma entre todos os numeros impares que sao multiplos de 3
 # e que se encontram no intervalo de 1 ate 500
 soma = 0
-print('numeros primos divisiveis por 3')
+impares = 0
+print('numeros impares divisiveis por 3')
 for c in range(1, 500+1):
-    if c % 3 == 0:
-        soma += c
-print('A soma de todos os impares sao: {}'.format(soma))
+    if c % 2 != 0:
+        if c % 3 == 0:
+            soma += c
+            impares += 1
+print('A soma de todos os {} impares sao: {}'.format(impares, soma))
 print('fim teste')
