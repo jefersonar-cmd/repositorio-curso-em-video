949fc7d9967acbe205212cce22b4332f55cfa8a9
jefersonar-cmd <jefersonaraujo86@gmail.com>
2021-02-17 19:58:06 -0300

    Aula 15 Reajustada

--- python_mundo2/Aulas/aula15.py
+++ python_mundo2/Aulas/aula15.py
@@ -1,9 +1,16 @@
-cont = 1
+#cont = 1
+#while True:
+#    print(cont, '-> ', end='')
+#    cont += 1
+#    if cont < 10+1:
+#        continue
+#    else:
+#        break
+#print('Acabou')
+n = s = 0
 while True:
-    print(cont, '-> ', end='')
-    cont += 1
-    if cont < 10+1:
-        continue
-    else:
+    n = int(input('Digite um numero: '))
+    if n == 999:
         break
-print('Acabou')
+    s += n
+print(f'A soma vale {s}')
