3b7e6a0bfa0f8429cc93fddd871801c1785bd733
jefersonar-cmd <jefersonaraujo86@gmail.com>
2021-05-18 23:05:32 -0300

    desafio 72 concluída

--- python_mundo3/Desafios/desafio72.py
+++ python_mundo3/Desafios/desafio72.py
@@ -3,3 +3,12 @@
 
 #Seu programa deverá ler um número pelo teclado
 #(entre 0 e 20) e mostrá-la por extenso
+extenso = ('zero', 'um', 'dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
+while True:
+    valor = int(input('Digite um valor (de 0 à 20): '))
+    if valor >= 0 and valor <= 20:
+        print(f'O valor {valor} por extenso é {extenso[valor]}')
+        break
+    else:
+        print(f'Número {valor} inexistente')
+        continue
