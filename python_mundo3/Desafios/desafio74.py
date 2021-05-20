f9254d49ea5722f772137dbe3fa71b05b51c9601
jefersonar-cmd <jefersonaraujo86@gmail.com>
2021-05-19 23:28:42 -0300

    desafio 74 concluída

--- python_mundo3/Desafios/desafio74.py
+++ python_mundo3/Desafios/desafio74.py
@@ -2,3 +2,15 @@
 #colocar em uma TUPLA
 #Depois disso, mostre a LISTAGEM DE NUMEROS
 #gerados e também indique o MENOR e o MAIOR valor que estão na tupla.
+from random import randint
+tupla = ()
+valor = 0
+print('Atividade: Tupla registradas aleatóriamente')
+for c in range(1, 6):
+    valor = randint(0, 100)
+    tupla += (valor,)
+print('A) Listagem:')
+for c in range(0, len(tupla)):
+    print(f'{tupla[c]}')
+print('B) Maior e menor:')
+print(f'O maior número é {max(tupla)}\nE o menor número é {min(tupla)}')
