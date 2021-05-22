a910c7c933480df3e68b3d977fdc788ca1fcc7f7
jefersonar-cmd <jefersonaraujo86@gmail.com>
2021-05-22 16:56:30 -0300

    desafio 77 concluída

--- python_mundo3/Desafios/desafio77.py
+++ python_mundo3/Desafios/desafio77.py
@@ -2,3 +2,9 @@
 #com VARIOS PALAVRAS (não usar acentos).
 #Depois disso, você deve mostrar, para cada palavra,
 #quais são as suas VOGAIS
+palavras = ('valor', 'adquirir', 'proteger', 'moeda', 'digital', 'contra', 'estatal')
+for p in palavras:
+    print(f'\nNa plavra {p} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
