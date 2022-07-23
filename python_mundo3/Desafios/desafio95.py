3c4da08d29e8c0ade3089a15afbbaa751a6b0765
jefersonar-cmd <jefersonaraujo86@protonmail.com>
2022-07-22 23:49:40 -0300

    Exercicio 95 Finalizado com Seu Celso Ku Cossano

--- python_mundo3/Desafios/desafio95.py
+++ python_mundo3/Desafios/desafio95.py
@@ -3,30 +3,43 @@
 from string import whitespace
 from time import sleep
 jogador = {}
-aproveitamento = []
-qtd = 0
+jogadores = {}
+ids = 1
 while True:
-    qtd += 1
-    jogador['id'] = qtd
-    jogador['nome'] = str(input('Informe Jogador: '))
-    jogador['partidas'] = int(input('Informe quantidades de partidas: '))
+    jogador['nome'] = str(input('Informe o Nome do Jogador: '))
+    jogador['partidas'] = int(input('Informe Quantidade de Partidas: '))
+    partidas = jogador['partidas']
     gols = []
-    gtotal = gol = 0
-    for g in range(0, jogador['partidas']):
-        gol = int(input(f'Gols na {g+1}ª Partida: '))
-        gols.append(gol)
-        gtotal += gol
+    gtotal = 0
+    for g in range(0, partidas):
+        gols.append(int(input(f'Informe QTD de Gols da {g+1}ª Partida: ')))
     jogador['gols'] = gols
+    for g in range(0, partidas):
+        gtotal += gols[g]
     jogador['gtotal'] = gtotal
-    aproveitamento.append(jogador.copy())
+    jogadores[ids] = jogador.copy()
+    ids += 1
     jogador.clear()
-    if str(input('Deseja continuar? [S/N]: ')).upper() == 'N':
+    if str(input('Deseja adicionar mais?[S/N] ')).upper() == 'N':
         break
     else:
         continue
 
-print('Verificando dados...')
-sleep(3)
-print(f'{"id":<4}{"nome":>8}{"Gols":>8}{"GTotal":>8}')
-for a in range(0, len(aproveitamento)):
+print('Calculando...')
+sleep(2)
+print(f"{'ID':<4}{'Nome':<8}{'Partidas':<12}{'Gols':<8}")
+for i, j in jogadores.items():
+    print(f"{i:<4}{j['nome']:<8}{j['partidas']:<12}{j['gtotal']:<8}")
+while True:
+    idper = int(input(f'Deseja ver mais detalhes de qual jogador?\nDigite o ID ou 999 para finalizar o programa\nR: '))
+    if idper == 999:
+        break
+    else:
+        print(f"ID: {idper}\nNome: {jogadores[idper]['nome']}\nPartidas: {jogadores[idper]['partidas']}")
+        gols = jogadores[idper]['gols']
+        for g in range(0, len(gols)):
+            print(f"    Gols da {g+1}ª Partida: {gols[g]}")
+        print(f"Total de Gols no Campeonato: {jogadores[idper]['gtotal']}")
+        continue
 
+print('Finalizando programa.\nObrigado por Utilizar! ^-^/')
