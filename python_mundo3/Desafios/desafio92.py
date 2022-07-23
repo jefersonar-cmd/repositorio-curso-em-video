dec543a0b7acc436422500a1aa4ddc842ad0aee2
jefersonar-cmd <jefersonaraujo86@protonmail.com>
2022-07-23 14:02:01 -0300

    Update no desafio 92

--- python_mundo3/Desafios/desafio92.py
+++ python_mundo3/Desafios/desafio92.py
@@ -1,7 +1,7 @@
 # crie um programa que leia NOME, ANO DE NASCIMENTO CTPS e cadastre-os (com IDADE) em um DICIONARIO
 # se por acaso a CTPS for diferente de ZERO, o dicionario recebera também o ANO DE CONTRATACAO
 # e o SALARIO. calcule e acrescente, alem da IDADE, com quantos anos a pessoa vai se APONSENTAR.
-import time
+from datetime import datetime
 aposentadoria = {}
 aposentadoria['nome'] = str(input('Digite o nome: '))
 aposentadoria['nascimento'] = int(input('Digite o ano de nascimento: '))
@@ -10,7 +10,7 @@ if aposentadoria['ctps'] == 0:
     for i, v in aposentadoria.items():
         print(f'{i}: {v}')
 else:
-    idade = time.localtime().tm_year
+    idade = datetime.now().year
     aposentadoria['idade'] = idade - aposentadoria['nascimento']
     aposentadoria['aposentadoria'] = aposentadoria['idade'] + 35
 
