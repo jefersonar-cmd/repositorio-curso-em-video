3a15ffc53ad16e30541e67a12f6767cb3d46fcec
Jefferson Santos Araújo <71133431+jefersonar-cmd@users.noreply.github.com>
2022-08-23 01:21:48 -0300

    Desafio 101 refeito

--- python_mundo3/Desafios/desafio101.py
+++ python_mundo3/Desafios/desafio101.py
@@ -1,13 +1,11 @@
 # Crie um programa que tenha um FUNÇÃO chamada voto() que vai receber um PARAMETRO o ANO DE NASCIMENTO de uma pessoa, RETORNANDO um valor LITERAL indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATORIO nas eleições.
-from datetime import datetime
-
-
 def voto(ano):
     """
     -> verificar se o cidadão é obrigado ou não a votar... <-
     :param ano: recebe o ano de nascimento
     :return: retorna NEGADO se idade menor que 16 anos, OBRIGATORIO se entre 18 à 69 anos ou OPCIONAL caso entre 16, 17 ou acima dos 70 anos
     """
+    from datetime import datetime
     atual = datetime.now().year
     idade = atual - ano
     if idade >= 16 and idade <= 17 or idade >= 70:
